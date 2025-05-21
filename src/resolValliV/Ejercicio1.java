ef8b3b75b37c8e24edf7b43e2d156e157078a2aa
VykoV <vallivictoria@gmail.com>
2025-05-21 19:50:18 -0300

    Inicio de TP2

---
+++ src/resolValliV/Ejercicio1.java
@@ -0,0 +1,34 @@
+package resol.ValliV;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class Ejercicio1 {
+    public void ejercicio1(){
+        Scanner scanner = new Scanner(System.in);
+
+        DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+
+        try{
+            System.out.println("Ingrese primera fecha y hora: 'dd/MM/yyyy HH:mm' ");
+            String fechaUser1= scanner.nextLine();
+            LocalDateTime fecha1 = LocalDateTime.parse(fechaUser1, formato);
+
+            System.out.println("Ingrese segunda fecha y hora: 'dd/MM/yyyy HH:mm' ");
+            String fechaUser2= scanner.nextLine();
+            LocalDateTime fecha2 = LocalDateTime.parse(fechaUser2, formato);
+
+            if(fecha1.isAfter(fecha2)){
+                System.out.println(fecha1 + " es mayor a " + fecha2 );
+            }else if(fecha1.isBefore(fecha2)){
+                System.out.println(fecha2 + " es mayor a " + fecha1 );
+            } else {
+                System.out.println("Son la misma fecha");
+            }
+        }catch (Exception e){
+            System.out.println("Ingrese el formato correctamente");
+        }
+
+    }
+}
---
+++ src/resolValliV/Principal.java
@@ -0,0 +1,34 @@
+package resol.ValliV;
+
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("¿Que programa quiere ejecutar?" +
+                "\n01. Ejercicio 1" +
+                "\n02. Ejercicio 2" +
+                "\n03. Ejercicio 3" +
+                "\n04. Ejercicio 4" +
+                "\n05. Ejercicio 5" +
+                "\n06. Ejercicio 6" +
+                "\n07. Ejercicio 7" +
+                "\n08. Ejercicio 8" +
+                "\n09. Ejercicio 9" +
+                "\n10. Ejercicio 10" +
+                "\n11. Salir");
+        int num = scanner.nextInt();
+
+        switch (num){
+            case 1:
+                Ejercicio1 ejercicio1 = new Ejercicio1();
+                ejercicio1.ejercicio1();
+            case 11:
+                System.exit(0);
+            default:
+                System.out.println("Intente ingresas nuevamente un numero");
+                main(args);
+        }
+    }
+}
